1f56d97695e0277640ffe794162ad80ff45625fd
Pablo Marrero <pablo@aquait.com.uy>
2013-10-03 14:48:35 -0300

    Initial commit for the autogenerate project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BasicSkeleton::Application.config.secret_key_base = '7c924cba4ba64eab875e334961de383d75cb6c5225371c541549d5bf992ad56c55d184e0cdba20b29c22caa729536fd5bb17201544157f18c7172d620a6bf6ac'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BasicSkeleton::Application.config.session_store :cookie_store, key: '_basic_skeleton_session'
