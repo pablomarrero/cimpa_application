dccae68292154d6f42873b193b354a3aa8db7cd4
pablomarrero <pablo@aquait.com.uy>
2013-10-10 18:14:45 -0200

    Apply patch to cancan for Rails 4

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_filter :authenticate_user!
+  before_filter do
+    resource = controller_name.singularize.to_sym
+    method = "#{resource}_params"
+    params[resource] &&= send(method) if respond_to?(method, true)
+  end
 
 end
