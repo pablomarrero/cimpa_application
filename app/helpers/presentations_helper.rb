1af3be25393e0ef154373dd59b7e366dfdfc3fef
Pablo Marrero <pablo.marrero@gmail.com>
2014-06-17 00:19:19 -0300

    ...

--- app/helpers/presentations_helper.rb
+++ app/helpers/presentations_helper.rb
@@ -2,4 +2,7 @@ module PresentationsHelper
 	def open_new_presentations?
 		DateTime.now < Date.strptime('20140615','%Y%m%d')
 	end
+	def open_final_presentations?
+		DateTime.now < Date.strptime('20140815','%Y%m%d')
+	end
 end
