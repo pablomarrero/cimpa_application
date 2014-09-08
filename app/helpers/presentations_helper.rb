3be89fdf3dc128283988478813067bf9ce7e7d0e
Alfonso Perez <chubak@gmail.com>
2014-09-08 04:08:43 +0200

    deadline for open_new_presentations and open_final_presentations to 1st october

--- app/helpers/presentations_helper.rb
+++ app/helpers/presentations_helper.rb
@@ -3,10 +3,10 @@ module PresentationsHelper
 		DateTime.now < Date.strptime('20140615','%Y%m%d')
 	end
 	def open_new_presentations?
-		DateTime.now < Date.strptime('20140815','%Y%m%d')
+		DateTime.now < Date.strptime('20141001','%Y%m%d')
 	end
 	def open_final_presentations?
-		DateTime.now < Date.strptime('20140815','%Y%m%d')
+		DateTime.now < Date.strptime('20141001','%Y%m%d')
 	end
 	def need_modification? presentation
 		need_modification1?(presentation) || need_modification2?(presentation)
